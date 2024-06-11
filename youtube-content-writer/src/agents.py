eeff806aeed4e04b464d4ceb5b8fd95421213df7
sinanazem <sinanaazem@gmail.com>
2024-06-11 16:55:37 +0330

    add multi agent llm project youtube-content-writer

---
+++ youtube-content-writer/src/agents.py
@@ -0,0 +1,48 @@
+from crewai import Agent
+
+from langchain_openai import ChatOpenAI
+import os
+
+# Set environment variables
+os.environ['OPENAI_API_BASE'] = 'http://localhost:11434/v1'
+os.environ['OPENAI_MODEL_NAME'] = 'openhermes'
+os.environ['OPENAI_API_KEY'] = 'NA'
+
+llm = ChatOpenAI(
+    model = "crewai-llama2",
+    base_url = "http://localhost:11434/v1")
+
+
+
+## Create a senior blog content researcher
+blog_researcher = Agent(
+    role = 'Blog Researcher from Youtube Videos',
+    goal = 'get the relevant video transcription for the topic {topic} from the provided Yt channel',
+    verbose=True,
+    memory=True,
+    backstory=(
+       "Expert in understanding videos in AI Data Science , Machine Learning And GEN AI and providing suggestion" 
+    ),
+    llm = llm,
+    tools=[],
+    allow_delegation=True
+)
+
+
+## creating a senior blog writer agent with YT tool
+blog_writer = Agent(
+    role='Blog Writer',
+    goal='Narrate compelling tech stories about the video {topic} from YT video',
+    verbose=True,
+    memory=True,
+    backstory=(
+        "With a flair for simplifying complex topics, you craft"
+        "engaging narratives that captivate and educate, bringing new"
+        "discoveries to light in an accessible manner."
+    ),
+    llm = llm,
+    tools=[],
+    allow_delegation=False
+
+
+)
